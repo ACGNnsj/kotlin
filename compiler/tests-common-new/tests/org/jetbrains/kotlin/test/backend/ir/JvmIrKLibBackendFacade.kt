7f0c20e22681f1abb44b4c48ee246ace11321cfc
Ivan Kochurkin <ivan.kochurkin@jetbrains.com>
2022-08-14 21:52:06 +0200

    [Testing] Introduce KLibGenerator and JvmIrKLibBackendFacade

---
+++ compiler/tests-common-new/tests/org/jetbrains/kotlin/test/backend/ir/JvmIrKLibBackendFacade.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2010-2022 JetBrains s.r.o. and Kotlin Programming Language contributors.
+ * Use of this source code is governed by the Apache 2.0 license that can be found in the license/LICENSE.txt file.
+ */
+
+package org.jetbrains.kotlin.test.backend.ir
+
+import org.jetbrains.kotlin.test.model.*
+import org.jetbrains.kotlin.test.services.TestServices
+
+class JvmIrKLibBackendFacade(
+    val testServices: TestServices,
+) : AbstractTestFacade<KLibArtifact, BinaryArtifacts.Jvm>() {
+    private val jvmFacadeHelper = JvmBackendFacadeHelper(testServices)
+
+    override val inputKind = KLibKinds.KLib
+    override val outputKind = ArtifactKinds.Jvm
+
+    override fun shouldRunAnalysis(module: TestModule): Boolean {
+        val incomingDependencies = testServices.dependencyProvider.getIncomingDependencies(module)
+        return incomingDependencies.none { it.relation == DependencyRelation.DependsOnDependency }
+    }
+
+    override fun transform(module: TestModule, inputArtifact: KLibArtifact): BinaryArtifacts.Jvm? {
+        require(inputArtifact is KLibArtifact.JvmIrKLibArtifact) {
+            "JvmIrKLibBackendFacade expects KLibArtifact.JvmIrKLibArtifact as input"
+        }
+
+        return jvmFacadeHelper.transform(
+            inputArtifact.state,
+            inputArtifact.codegenFactory,
+            inputArtifact.backendInput,
+            inputArtifact.sourceFiles,
+            module
+        )
+    }
+}
